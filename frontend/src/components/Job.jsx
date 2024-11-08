a44393ba77c9e123864fcb60799fa3e6f79328fd
Aleksandr <134843809+fireek1@users.noreply.github.com>
2024-11-09 01:22:10 +0200

    сверстал чутка

---
+++ frontend/src/components/Job.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Job() {
+    return (
+        <div className="Job">
+            
+        </div>
+    )
+}
+
+export default Job
---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import './../styles/Main.css'
+import date from './../images/calendar.svg'
+import arrow from './../images/arrow.svg'
+
+function Main() {
+    return (
+    <div className="Main">
+        <div className="Main_title">
+            <h2 className="Main_titleText">Привет, Никита!</h2>
+            <div className="Main_titleDate">
+                <img src={date} alt="Calendar" className="Main_calendar" />
+                <p className="Main_textDate">8 нояб. - 16 нояб. 2024</p>
+                <img src={arrow} alt="Arrow" className="Main_arrow" />
+            </div>
+        </div>
+        
+    </div>
+    )
+}
+
+export default Main
