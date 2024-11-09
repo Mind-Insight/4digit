092a9531df39ddb12b3ec2184ca0472594f69e9d
Aleksandr <134843809+fireek1@users.noreply.github.com>
2024-11-09 03:26:24 +0200

    add parsing

--- ai/annotations.py
+++ ai/annotations.py
@@ -2,9 +2,28 @@ import pickle
 
 
 def load_pkl_file(file_path):
-    with open(file_path, "rb") as f:
+    with open(file_path, "rb") as f, open("dataset/transcription_training.pkl", "rb") as trans:
         data = pickle.load(f, encoding="latin1")
-    return data
+        data_trans = pickle.load(trans)
+    return (data, data_trans)
 
 
-annotations = load_pkl_file("dataset/annotation_training.pkl")
+annotations, transcriptions = load_pkl_file("dataset/annotation_training.pkl")
+
+parse = {}
+
+for key in annotations:
+    for key1 in annotations[key]:
+        parse[key1] = []
+    break
+
+for key in annotations:
+    for key1 in annotations[key]:
+        if key == 'interview':
+            continue
+        parse[key1].append(annotations[key][key1])
+
+for key in transcriptions:
+    parse[key].append(transcriptions[key])
+
+print(parse)
